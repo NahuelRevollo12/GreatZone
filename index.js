2d33e4aa1f076c70dbbb9e0fc5d012d081e4d986
Nahuel Revollo <seaquesto@hotmail.es>
2022-09-13 13:56:51 -0300

    Implementando DOM basico

--- index.js
+++ index.js
@@ -39,11 +39,22 @@ const graficas = [
         modelo: "RTX",
         precio: 900
     },
+    /* {
+        id: 5,
+        nombre: "3080",
+        estado: "Nuevo",
+        modelo: "RTX",
+        precio: 1200
+    }, */
+
+    
 ];
+
+// const graficas5 = new graficas("3080", "./img/msi-rtx-3080-gaming-z-trio-10g.jpg", "RTX", 1200 , "https://www.youtube.com/watch?v=7d_jQycdQGo")
 
 let menu = "Las graficas en Stock son:\n";
 
-graficas.forEach((grafica)=>{
+graficas.forEach((grafica) => {
     console.log(grafica);
     menu += `${grafica.id}) esta grafica es la mejor ${grafica.nombre} ${grafica.modelo}\n`;
 })
@@ -52,12 +63,12 @@ menu += "0) Salir";
 let option;
 const carrito = [];
 
-function seleccionarGrafica(){
+function seleccionarGrafica() {
     while (option !== 0) {
         option = Number(prompt(menu));
-        
-        let productoEncontrado = graficas.find((grafica)=>grafica.id == option);
-        
+
+        let productoEncontrado = graficas.find((grafica) => grafica.id == option);
+
         if (productoEncontrado == undefined) {
             alert("Gracias, vuelva pronto");
         } else {
@@ -65,21 +76,90 @@ function seleccionarGrafica(){
             agregarAlCarrito(productoEncontrado)
         }
     }
-    
+
 }
 
 
 function agregarAlCarrito(grafica) {
     carrito.push(grafica);
     console.log(carrito);
-}
 
+    
+
+}
+let greatZone = document.createElement("p");
+    greatZone.innerHTML = "Los mejores descuento de hasta un 10% en tu primera compra"
+    document.body.append(greatZone);
 
 function getLastID() {
     return graficas.length;
-} 
+}
+
+/* function mostrarGraficas(graficas) {
+
+    const contenedorDeGraficas = document.getElementById("contenedor-de-graficas"); //<div id="contenedor-de-graficas"></div>
+    contenedorDeGraficas.innerHTML = "";
+    // por cada uno
+    graficas.forEach(grafica => {
+        const divGrafica = document.createElement("div");
+        divGrafica.classList.add("graficas");
+        divGrafica.innerHTML = `
+        <h3>Las Mejores graficas con 10% de Descuento</h3>
+        <p>Modelo: ${grafica.id}</p>
+      `;
+        contenedorDeGraficas.append(divGrafica);
+    })
+} */
+
+// mostrarGraficas(graficas);
+
+
+
+
+
+
+
+
 
 seleccionarGrafica();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log("***Elementos del Documento***");
+// console.log(window.document);
+console.log(document);
+console.log(document.getElementsByTagName("h5"));
+
+let elementosP =
+    document.getElementsByTagName("h5");
+
+console.log("Las graficas disponibles son",
+    elementosP[0].innerText + ", " +
+    elementosP[1].innerText + ", " +
+    elementosP[2].innerText + " y " +
+    elementosP[3].innerText + ".");
+
+
+
+
 
 
 
