6f93a51afd01d9c389698337a52f91952cf5bafe
Nahuel Revollo <seaquesto@hotmail.es>
2022-09-15 03:16:13 -0300

    aplicando carrito

--- index.js
+++ index.js
@@ -1,4 +1,124 @@
-function solicitarNombre() {
+let stockProductos = [
+    {id: 1, nombre: "RTX 3060", cantidad: 1, desc: "Buenisima para jugar lo que sea", precio: 12000, gama: "low", img: './imagenes/rtx-3060.jpg'},
+    {id: 2, nombre: "RTX 3070", cantidad: 1, desc: "Buenisima para jugar lo que sea", precio: 13000, gama: "medium", img: './imagenes/rtx-3070.jpg'},
+    {id: 3, nombre: "RTX 3080", cantidad: 1, desc: "Buenisima para jugar lo que sea", precio: 14000, gama: "high", img: './imagenes/rtx-3080.jpg'},
+    {id: 4, nombre: "RTX 3090", cantidad: 1, desc: "Buenisima para jugar lo que sea", precio: 15000, gama: "high", img: './imagenes/rtx-3090.jpg'},
+]
+
+const contenedorProductos = document.getElementById('contenedor-productos')
+const contenedorCarrito = document.getElementById('carrito-contenedor')
+const botonVaciar = document.getElementById('vaciar-carrito')
+const contadorCarrito = document.getElementById('contadorCarrito')
+const precioTotal = document.getElementById('precioTotal')
+
+
+let carrito = []
+
+
+
+botonVaciar.addEventListener('click', () =>{
+    carrito.length = 0
+    actualizarCarrito()
+})
+
+stockProductos.forEach((producto) =>{
+    const div = document.createElement('div')
+    div.classList.add('producto')
+    div.innerHTML = `
+    <img src=${producto.img} alt="Grafica">
+    <h3>${producto.nombre}</h3>
+    <p>${producto.desc}</p>
+    <p>Gama: ${producto.gama}</p>
+    <p class="precioProducto">Precio:$ ${producto.precio}</p>
+    <button id="agregar${producto.id}" class="boton-agregar">Agregar <i class="fas fa-shopping-cart"></i></button>`
+    
+    contenedorProductos.appendChild(div)
+
+    const boton = document.getElementById(`agregar${producto.id}`)
+
+    boton.addEventListener('click', () => {agregarAlCarrito(producto.id)
+    })
+})
+
+const eliminarDelCarrito = (prodId) => {
+    const item = carrito.find((prod) => prod.id === prodId)
+    const indice = carrito.indexOf(item)
+    carrito.splice(indice, 1)
+    actualizarCarrito()
+}
+
+
+const agregarAlCarrito = (prodId) => {
+    const item = stockProductos.find((prod) => prod.id === prodId)
+    carrito.push(item)
+    actualizarCarrito()
+    console.log(carrito)
+}
+
+const actualizarCarrito = () => {
+    contenedorCarrito.innerHTML = ""
+
+    carrito.forEach((prod) =>{
+        const div = document.createElement('div')
+        div.className = ('productoEnCarrito')
+        div.innerHTML = `
+        <p>${prod.nombre}</p>
+        <p>Precio: ${prod.precio}</p>
+        <p>Cantidad: <span id="cantidad">${prod.cantidad}</span></p>
+        <button onclick="eliminarDelCarrito(${prod.id})" class="boton-eliminar"><i class="fas fa-trash-alt"></button>`
+
+        contenedorCarrito.appendChild(div)
+
+    })
+    contadorCarrito.innerHTML = carrito.length
+    precioTotal.innerHTML = carrito.reduce((acc, prod) => acc + prod.precio, 0)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* function solicitarNombre() {
     alert("Bienvenido a GreatZone Venta de las mejores Graficas")
     let nombre = prompt("Ingrese su nombre");
     while (nombre === "") {
@@ -51,4 +171,4 @@ solicitarNombre();
 let miProducto = mostrarProductos();
 let precioProducto = validarPrecio(miProducto);
 console.log(precioProducto);
-cobrar(miProducto, precioProducto)
+cobrar(miProducto, precioProducto) */
---
+++ stock.js
@@ -0,0 +1,6 @@
+let stockProductos = [
+    {id: 1, nombre: "RTX 3060", cantidad: 1, desc: "Buenisima para jugar lo que sea", precio: 12000, gama: "low", img: './img/rtx-3060.jpg'},
+    {id: 2, nombre: "RTX 3070", cantidad: 1, desc: "Buenisima para jugar lo que sea", precio: 13000, gama: "medium", img: './img/rtx-3070.jpg'},
+    {id: 3, nombre: "RTX 3080", cantidad: 1, desc: "Buenisima para jugar lo que sea", precio: 14000, gama: "high", img: './img/rtx-3080.jpg'},
+    {id: 4, nombre: "RTX 3090", cantidad: 1, desc: "Buenisima para jugar lo que sea", precio: 15000, gama: "high", img: './img/rtx-3090.jpg'},
+]
